86f9776444f3d4c890d354d182152bb09b9b8869
tobycm <newtobycm@gmail.com>
2022-09-01 15:43:49 -0700

    fix code lmao

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ import config
 
 token = config.token
 prefix = ['a.', 'A.']
-bot = commands.Bot(command_prefix=prefix, intents=Intents.all(), help_command=None)
+bot = commands.Bot(command_prefix=prefix, intents=Intents.all())
 
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=Game(name="prefix is a. or A."))
     for file in os.listdir('./cogs'):
         if file.endswith('.py'):
             await bot.load_extension("cogs." + file[:-3])
@@ -30,5 +29,16 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         return
     raise error
+
+async def startup_tasks():
+    """
+    Run after bot is ready
+    """
+
+    await bot.wait_until_ready()
+
+    await bot.change_presence(activity=Game(name="prefix is a. or A."))
+
+bot.loop.create_task(startup_tasks())
 
 bot.run(token)
