37da3520fc37a75b3b365bb7011c32b341a56c7c
Water Dragon <108056996+hoangdev09@users.noreply.github.com>
2022-09-01 16:53:45 +0700

    Remove data in file

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # DATABASE of Redis
-host = 'redis-11176.c292.ap-southeast-1-1.ec2.cloud.redislabs.com'
-port = 11176
-username = 'default'
-password = 'zqPQ3wXqXdWTEwOHJ9yXCqnLMeKKUDrl'
+host = ''
+port = ''
+username = ''
+password = ''
 
 # DISCORD
-token = 'OTk5OTYxOTY1Njk2OTI5ODMy.GuwINV.QCk1BgxxeJXtqJdRVEuKhuXkFHGIqYvD8cjgd0'
+token = ''
